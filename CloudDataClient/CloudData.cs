de15ef4b8579578000c059f834ba05b3e4c660ae
Israel Lopez <israel@ilopez.com>
2015-08-06 20:07:43 -0700

    First Working Version

---
+++ CloudDataClient/CloudData.cs
@@ -0,0 +1,53 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudDataClient
+{
+    public class CloudData
+    {
+        private RestClient web;
+        public CloudData(String URLBase, String APIKey)
+        {
+            this.web = new RestClient(URLBase);
+            this.web.Authenticator = new HttpBasicAuthenticator(APIKey, APIKey);
+        }
+        public QueryResponse Query(String Query)
+        {
+            var rq = GenerateRequest(Query);
+
+            var rs = this.web.Execute(rq);
+            int i = 0;
+            QueryResponse qrs = new QueryResponse();
+            qrs.HTTPCode = (int)rs.StatusCode;
+            qrs.RawContent = rs.Content;
+            return qrs;
+        }
+
+        private static RestRequest GenerateRequest(String Query)
+        {
+            var rq = new RestRequest("simplequery", Method.POST);
+            rq.AddParameter("text/xml", Query, ParameterType.RequestBody);
+            return rq;
+        }
+
+        public T Query<T>(String Query) where T : new ()
+        {
+            var rq = GenerateRequest(Query);
+            var rs = this.web.Execute<T>(rq);
+            if (rs.ErrorException != null)
+            {
+                throw new Exception("Query Exception: " + rs.ErrorException);
+            }
+            return rs.Data;
+        }
+    }
+    public class QueryResponse
+    {
+        public int HTTPCode { get; set; }
+        public String RawContent { get; set; }       
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using CloudDataClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudData cd = new CloudData("http://opencart.israellopezconsulting.com/fishbowl/", "DLVJvVSnjuOHresulfD4");
+
+            var rs = cd.Query("select * from oc_country limit 10");
+            Console.WriteLine(rs.RawContent);
+
+            var obj = cd.Query<List<TestObject>>("select * from oc_country limit 10");
+
+            Console.WriteLine("TEST");
+        }
+    }
+
+    public class TestObject
+    {
+        public string country_id { get; set; }
+        public string name { get; set; }
+        public string iso_code_2 { get; set; }
+        public string iso_code_3 { get; set; }
+        public string address_format { get; set; }
+        public string postcode_required { get; set; }
+        public string status { get; set; }
+    }
+}
